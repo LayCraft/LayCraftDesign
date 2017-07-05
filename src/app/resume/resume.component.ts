16a2ef402553d5e322e7a065d835648c122be642
Curtis LayCraft <curtis.laycraft@zoho.com>
2017-07-05 13:03:22 -0700

    Added Json data source for resume.

---
+++ src/app/resume/resume.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resume',
+  templateUrl: './resume.component.html',
+  styleUrls: ['./resume.component.css']
+})
+export class ResumeComponent implements OnInit {
+  title: string;
+  data: any;
+  constructor() {}
+
+  ngOnInit() {
+  }
+}
