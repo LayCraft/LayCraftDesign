60390c113a399429174900cb40d20a7689eaa0f4
Curtis LayCraft <curtis.laycraft@zoho.com>
2017-07-13 13:09:46 -0700

    Converted json to TS file. Need to troubleshoot length in long description.

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ResumeDataService } from '../resume-data.service';
 
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css']
 })
+
 export class ResumeComponent implements OnInit {
   title: string;
   data: any;
-  constructor() {
+  constructor(private resumeData: ResumeDataService) {
     this.title = 'Résumé';
+    this.data = ResumeDataService.length;
   }
 
   ngOnInit() {
---
+++ src/app/resume/resumeInfo.ts
@@ -0,0 +1,157 @@
+export const RESUME = [
+  { name: 'Curtis LayCraft' },
+  { phone: '+ 1 (250) 891-5376' },
+  { email: 'Curtis.LayCraft@zoho.com' },
+  { objective: 'To find and join a great team.' },
+  { education: [
+    [
+      { institution: 'Lethbridge College' },
+      { dates: '2003-2004' },
+      { program: 'Business Administration' },
+      { designation: '' },
+      { credential: 'Business Administration Diploma with Marketing Major' }
+    ],
+    [
+      { institution: 'Edmonton Digital Arts College'},
+      { dates: '2006-2007' },
+      { program: 'Graphic Design'},
+      { credential: 'Digital Media Producer Diploma'},
+      { designation: '' }
+    ],
+    [
+      { institution: 'Camosun College'},
+      { dates: '2015-2017'},
+      { program: 'Computer Systems Technology'},
+      { credential: 'Computer Systems Technology Diploma'},
+      { designation: 'Co-op'}
+    ]
+  ]},
+  { work: [
+    [
+      { jobNo: 0 },
+      { adult: false },
+      { employer: 'Calgary Herald'},
+      { position: 'Paper Carrier'},
+      { dates: '1992-1997'},
+      { shortDesc: 'Paper delivery and collections'},
+      { longDesc: 'Morning deliveries seven days a week and monthly collection.'}
+    ],
+    [
+      { jobNo: 1 },
+      { adult: false },
+      { employer: 'Claresholm Pharmacy IDA'},
+      { position: 'Assistant'},
+      { dates: '1995-2000'},
+      { shortDesc: 'Stocking shelves and cleanup'},
+      { longDesc: 'Restocking and refacing shelves, sweeping, vacuuming, and annual inventory. Part-time.'}
+    ],
+    [
+      { jobNo: 2 },
+      { adult: false },
+      { employer: 'Mill Creek Baptist Camp' },
+      { position: 'Kitchen Staff' },
+      { dates: '2000'},
+      { shortDesc: 'Cook meals for large groups'},
+      { longDesc: 'Food preperation and cooking at a summer camp. Serving three meals and two snacks to 50-60 people daily.' }
+    ],
+    [
+      { jobNo: 3 },
+      { adult: false},
+      { employer: 'Humpty\'s Family Restaurant'},
+      { position: 'Server' },
+      { dates: '1999' },
+      { shortDesc: 'Customer service and cleanup'},
+      { longDesc: 'Responsibilities included seating customers, serving beverages and food, cleaning tables, and making bills.'},
+    ],
+    [
+      { jobNo: 4 },
+      { adult: true },
+      { employer: 'Bridge Brand Food Services Ltd' },
+      { position: 'Warehouse' },
+      { dates: '2002-2003'},
+      { shortDesc: 'Build food orders and load trucks'},
+      { longDesc: 'Pick and load food onto trucks. Operation of heavy duty machinery and occasional delivery.'}
+    ],
+    [
+      { jobNo: 5 },
+      { adult: true},
+      { employer: 'Western Wats Research' },
+      { position: 'Telephone Interviewer'},
+      { dates: '2003'},
+      { shortDesc: 'Placing outgoing calls and data entry'},
+      { longDesc: 'Placing outgoing calls across the USA to collect information for political parties and national corporations.'}
+    ],
+    [
+      { jobNo: 6 },
+      { adult: true},
+      { employer: 'Starbucks Coffee Co.' },
+      { position: 'Barista'},
+      { dates: '2004-2006' },
+      { shortDesc: 'Slinging coffee, cash handling, and schmoozing with patrons' },
+      { longDesc: 'Slinging coffee, cash handling, and schmoozing with patrons. Planned live music events and community engagement.'}
+    ],
+    [
+      { jobNo: 7},
+      { adult: true},
+      { employer: 'AT&T (Convergys)'},
+      { position: 'Customer Service Representative'},
+      { dates: '2006'},
+      { shortDesc: 'Incoming technical support, billing, and activations'},
+      { longDesc: 'Tech-support, billing, activations and transfers. Troubleshoot cellular systems and devices. CRM Data entry.'}
+    ],
+    [
+      { jobNo: 8},
+      { adult: true},
+      { employer: 'LayCraft Design'},
+      { position: 'Founder, Graphic Designer, Client management' },
+      { dates: '2007-2015' },
+      { shortDesc: 'Graphic design, communication, and applied arts'},
+      { longDesc: 'LayCraft Design was a full service graphic design company. Responsibilities included client communication, administration, creative direction, print design, illustration, photography, branding consultation, and contract instruction.  Examples of clients: Government of Alberta computer training centre, Sun Media Group, BC Art Teachers Association, The Red Cross, The Salvation Army, and Events Edmonton. Created various branding, ads, promotions for nightclubs, restaurants, industrial companies, and other businesses.'}
+    ],
+    [
+      { jobNo: 9},
+      { adult: true},
+      { employer: 'Edmonton Digital Arts College' },
+      { position: 'Instructor' },
+      { dates: '2007-2009'},
+      { shortDesc: 'Corporate training and taught design software'},
+      { longDesc: 'Work was primarily corporate training. Provided training for various organizations: Blackberry and Acrobat training for the Government of Alberta, InDesign training for Sun Media Group, Adobe Creative Suite training for the University of Alberta\'s faculty of extension.'}
+    ],
+    [
+      { jobNo: 10 },
+      { adult: true },
+      { employer: 'NAIT' },
+      { position: 'Instructor' },
+      { dates: '2007-2009'},
+      { shortDesc: 'Curriculum development and instruction'},
+      { longDesc: 'Curriculum development and instruction at The Northern Alberta Institute of Technology. InDesign Level 1 & 2, Acrobat Level 1 & 2, Print Tools for Scanning & Printing, Photoshop Level 1-3, Digital Photo Basic, and Design Theory. Taught NAIT\'s kids summer camps for GameMaker, digital photography, and Photoshop.'}
+    ],
+    [
+      { jobNo: 11},
+      { adult: true},
+      { employer: 'The Salvation Army Community and Family Services'},
+      { position: 'Communication designer'},
+      { dates: '2011-2012'},
+      { shortDesc: 'Communication design, forms, and content creation.'},
+      {longDesc: 'Joomla development. Content Creation. Responsible for collecting and writing descriptions about various programs and services. Redesigned print materials forms, business cards, certificates, manuals, signs, and etc… Taught computer literacy skills in the computer lab: Windows, File Management, Word, Excel, and powerpoint. Responsible for various organizational tasks for Christmas Hamper and Toy Shop programs.'}
+    ],
+    [
+      { jobNo: 12},
+      { adult: true},
+      { employer: 'Interactivity Board Game Cafe' },
+      { position: 'Game Lord' },
+      { dates: '2013-2016' },
+      { shortDesc: 'Teaching games, cafe work, and business startup'},
+      { longDesc: 'Taught and sold games. Cafe duties. Responsible for technical installation and POS systems. Participation in the business startup. Board games are a beautiful mixture of algorithms, graphic design, game theory, and cardboard.'}
+    ],
+    [
+      { jobNo: 13},
+      { adult: true},
+      { employer: 'BC Public Service Agency'},
+      { position: 'Application Support'},
+      { dates: '2015-2016'},
+      { shortDesc: 'Six month co-op. Time and Leave Project'},
+      { longDesc: 'Six month co-op. Time and Leave Project; changeover to Oracle PeopleSoft. Report creation, data analysis, implementation, process recommendations, troubleshooting, strategic threat & risk assessment (STRA), privacy impact assessment (PIA), support for data migration. Specialization in implementation of the mobile version of MyHR.'}
+    ]
+  ]}
+];
